c227f7a01eeb5b7c181746da143399b38094d2cf
csfrancis94 <csfrancis@email.com>
2019-03-10 14:58:57 -0400

    Finished 2 endpoints for 24 hour notice!

--- Interview_API/src/main/java/com/revature/controllers/InterviewController.java
+++ Interview_API/src/main/java/com/revature/controllers/InterviewController.java
@@ -1,17 +1,33 @@
 package com.revature.controllers;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Arrays;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.revature.dtos.AssociateInterview;
+
 import com.revature.models.Interview;
+import com.revature.models.InterviewFeedback;
+import com.revature.models.InterviewFormat;
+import com.revature.models.FeedbackStatus;
+import com.revature.models.AssociateInput;
+
 import com.revature.services.InterviewService;
 
 @RestController
@@ -26,9 +42,143 @@ public class InterviewController {
 		return interviewService.findAll();
 	}
 	
+	@GetMapping("/page")
+	public Page<Interview> getInterviewPage(
+            @RequestParam(name="orderBy", defaultValue="id") String orderBy,
+            @RequestParam(name="direction", defaultValue="ASC") String direction,
+            @RequestParam(name="pageNumber", defaultValue="0") Integer pageNumber,
+            @RequestParam(name="pageSize", defaultValue="5") Integer pageSize) {
+		// Example url call: ~:8091/interview/page?pageNumber=0&pageSize=3
+		// The above url will return the 0th page of size 3.
+		Sort sorter = new Sort(Sort.Direction.valueOf(direction), orderBy);
+        Pageable pageParameters = PageRequest.of(pageNumber, pageSize, sorter);
+        
+        return interviewService.findAll(pageParameters);
+    }
+	
+	@GetMapping("reports/InterviewsPerAssociate/page")
+	public Page<AssociateInterview> getInterviewsPerAssociate(
+            @RequestParam(name="pageNumber", defaultValue="0") Integer pageNumber,
+            @RequestParam(name="pageSize", defaultValue="5") Integer pageSize) {
+		// Example url call: ~:8091/interview/page?pageNumber=0&pageSize=3
+		// The above url will return the 0th page of size 3.
+        Pageable pageParameters = PageRequest.of(pageNumber, pageSize);
+        
+        return interviewService.findInterviewsPerAssociate(pageParameters);
+    }
+	
+	//returns 2 numbers in a list
+	//the first is the number of users
+	//the second is the number of users who received 24 hour notice (according to the associate)
+	@GetMapping("reports/request24/associate/")
+	public List<Integer> getInterviewsWithin24HourNoticeAssociate() {
+		//find all interviews
+		List<Interview> allUsers = interviewService.findAll();
+		//return
+		List<Integer> returning;
+		//find all interviews where the users were notified in advance
+		ArrayList<Interview> allNotifiedUsers = new ArrayList<Interview>();
+		
+		//count all interviews
+		int countAll = allUsers.size();
+
+		//build a new list iteratively for allNotifiedUsers
+		for (Interview i : allUsers)
+		{
+			if (i.getAssociateInput() == null)
+				System.out.println("This interview has no associate input");
+			if (i.getAssociateInput() != null)
+				if (i.getAssociateInput().getReceivedNotifications() == null)
+					System.out.println("This interview has associate input but no received notifications date");
+			
+			//check of non null
+			if (i.getAssociateInput() != null)
+				//check of non null
+				if (i.getAssociateInput().getReceivedNotifications() != null)
+				{
+					//Singleton Calendar
+					Calendar cal = Calendar.getInstance();
+					//Set time on calendar to current receivedNotifications date
+					cal.setTime(i.getScheduled());
+					Date curDate = cal.getTime();
+					//Add 24 Hours to the current date
+					cal.add(Calendar.DATE, -1);
+					//Calculate a new date, one day from the receivedNotifications
+					Date oneDayBefore = cal.getTime();
+					//If meets criteria, push to new list
+					if (i.getAssociateInput().getReceivedNotifications().before(oneDayBefore) || !(i.getAssociateInput().getReceivedNotifications().after(oneDayBefore)))
+					{
+						allNotifiedUsers.add(i);
+					}
+			
+					System.out.println("getScheduled: "+i.getScheduled()+" oneDayBefore: "+oneDayBefore+" Associate: "+i.getAssociateInput().getReceivedNotifications());
+					System.out.println(i.getAssociateInput().getReceivedNotifications().before(oneDayBefore)+" vs "+(!(i.getAssociateInput().getReceivedNotifications().after(oneDayBefore))));
+				}
+		}
+		
+		//count only interviews that are within 24 hour notice
+		int countNotified = allNotifiedUsers.size();
+		returning = Arrays.asList(countAll, countNotified);
+        return returning;
+    }
+	
+
+	//returns 2 numbers in a list
+	//the first is the number of users
+	//the second is the number of users who received 24 hour notice (according to the manager)
+	@GetMapping("reports/request24/manager")
+	public List<Integer> getInterviewsWithin24HourNoticeManager() {
+		//find all interviews
+		List<Interview> allUsers = interviewService.findAll();
+		//return
+		List<Integer> returning;
+		//find all interviews where the users were notified in advance
+		ArrayList<Interview> allNotifiedUsers = new ArrayList<Interview>();
+		
+		//count all interviews
+		int countAll = allUsers.size();
+
+		//build a new list iteratively for allNotifiedUsers
+		for (Interview i : allUsers)
+		{
+			if (i.getNotified() == null)
+			{
+				System.out.println("This interview has no manager input");
+			}
+			
+			
+			//check of non null
+				if (i.getNotified() != null)
+				{
+					//Singleton Calendar
+					Calendar cal = Calendar.getInstance();
+					//Set time on calendar to current receivedNotifications date
+					cal.setTime(i.getScheduled());
+					Date curDate = cal.getTime();
+					//Add 24 Hours to the current date
+					cal.add(Calendar.DATE, -1);
+					//Calculate a new date, one day from the receivedNotifications
+					Date oneDayBefore = cal.getTime();
+					//If meets criteria, push to new list
+					if (i.getNotified().before(oneDayBefore) || !(i.getNotified().after(oneDayBefore)))
+					{
+						allNotifiedUsers.add(i);
+					}
+			
+					System.out.println("getScheduled: "+i.getScheduled()+" oneDayBefore: "+oneDayBefore+" Manager: "+i.getNotified());
+					System.out.println(i.getNotified().before(oneDayBefore)+" vs "+(!(i.getNotified().after(oneDayBefore))));
+				}
+				
+		}
+		
+		//count only interviews that are within 24 hour notice
+		int countNotified = allNotifiedUsers.size();
+		returning = Arrays.asList(countAll, countNotified);
+        return returning;
+    }
+	
 	@PostMapping("/saveInterview")
 	public Interview newInterview(@Valid @RequestBody Interview i) {
 		return interviewService.save(i);
 	}
-	
 }
--- Interview_API/src/main/java/com/revature/services/InterviewServiceImpl.java
+++ Interview_API/src/main/java/com/revature/services/InterviewServiceImpl.java
@@ -1,11 +1,19 @@
 package com.revature.services;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import com.revature.dtos.AssociateInterview;
 import com.revature.models.Interview;
 import com.revature.repos.InterviewRepo;
+import com.revature.utils.ListToPage;
 
 @Service
 public class InterviewServiceImpl implements InterviewService {
@@ -30,5 +38,40 @@ public class InterviewServiceImpl implements InterviewService {
 		// TODO Auto-generated method stub
 		return interviewRepo.findAll();
 	}
+	
+	@Override
+    public Page<Interview> findAll(Pageable page) {
+        // TODO Auto-generated method stub
+        return interviewRepo.findAll(page);
+    }
+
+	public List<AssociateInterview> findInterviewsPerAssociate() {
+		List<Interview> interviews = interviewRepo.findAll();
+		List<AssociateInterview> associates = new ArrayList<AssociateInterview>();
 		
+		for(Interview I: interviews) {
+			AssociateInterview A = new AssociateInterview(I);
+			int index = associates.indexOf(A);
+			System.out.println("New: " + A);
+			if(index>0) {
+				A=associates.get(index);
+				A.incrementInterviewCount();
+				associates.set(index, A);
+				System.out.println("Incremented: " + A);
+			} else {
+				associates.add(A);
+			}
+		}
+		Collections.sort(associates);
+		return associates;
+	}
+
+	public Page<AssociateInterview> findInterviewsPerAssociate(Pageable page) {
+//		List<AssociateInterview> associates = findInterviewsPerAssociate();
+//		int start = page.getPageNumber()*page.getPageSize();
+//		int end = ((page.getPageNumber()+1)*page.getPageSize())-1;
+//		return new PageImpl<AssociateInterview>(associates.subList(start, end), page, associates.size());
+		PageImpl PI = ListToPage.getPage(findInterviewsPerAssociate(), page);
+		return PI;
+	}
 }
